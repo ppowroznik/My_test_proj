5eb027d14450e676f9c0fce6209a0f9316678f6b
Pawel Powroznik <ppowroznik@gmail.com>
2024-04-27 14:33:01 +0200

    Initial Commit

---
+++ testProj/testProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjApp.swift
+//  testProj
+//
+//  Created by Paweł Powroźnik on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct testProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
