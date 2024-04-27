dc99eb23bebca1fe0ec0915a55440980d180251a
Pawel Powroznik <ppowroznik@gmail.com>
2024-04-27 09:22:33 +0200

    Initial Commit

---
+++ My_test_proj/My_test_projApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_test_projApp.swift
+//  My_test_proj
+//
+//  Created by Paweł Powroźnik on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct My_test_projApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
